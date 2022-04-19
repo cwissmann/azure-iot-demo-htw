01ae5ee50e9514926b989af73ccd9737bbaee4bf
cwissmann <christian.wissmann@zeiss.com>
2022-04-19 15:47:42 +0200

    update versions

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,11 +2,11 @@ terraform {
     required_providers {
         azurerm = {
         source  = "hashicorp/azurerm"
-        version = "~> 3.0.2"
+        version = "~> 3.2.0"
         }
         azuread = {
         source = "hashicorp/azuread"
-        version = "2.19.1"
+        version = "2.20.0"
      }
     }
     required_version = ">= 1.1.0"
