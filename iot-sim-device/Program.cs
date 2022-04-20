519e8c25f7e648dfab7a5584e37e98aea9cb3881
cwissmann <christian.wissmann@zeiss.com>
2022-04-20 09:54:34 +0200

    add simulated temperatr device

---
+++ iot-sim-device/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+
+var _random = new Random();
+var _messageId = 0;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello HTW");
+
+string deviceKey = "[DEVICEKEY]";
+string deviceId = "[DEVICEID]";
+string iotHubHostName = "[IOTHUBHOSTNAME]";
+var deviceAuthentication = new DeviceAuthenticationWithRegistrySymmetricKey(deviceId, deviceKey);
+
+
+DeviceClient deviceClient = DeviceClient.Create(iotHubHostName, deviceAuthentication, TransportType.Mqtt);
+
+while (true)
+{
+    double currentTemperature = 20 + _random.NextDouble() * 15;
+    double currentHumidity = 60 + _random.NextDouble() * 20;
+
+    var telemetryData = new {
+        messageId = _messageId++,
+        deviceId = deviceId,
+        temperature = currentTemperature,
+        humidity = currentHumidity
+    };
+
+    string messageString = JsonConvert.SerializeObject(telemetryData);
+    Message message = new Message(Encoding.ASCII.GetBytes(messageString));
+    message.Properties.Add("temperatureAlert", (currentTemperature > 30) ? "true" : "false");
+
+    await deviceClient.SendEventAsync(message);
+    Console.WriteLine($"{DateTime.Now} > Sending message: {messageString}");
+
+    await Task.Delay(2000);
+}
---
+++ iot-sim-device/README.md
@@ -0,0 +1,11 @@
+# Simulated IoT Device
+
+```
+dotnet new console
+
+dotnet add package Microsoft.Azure.Devices.Client --version 1.40.0
+```
+
+## References
+[Azure IoT SDK](https://github.com/Azure/azure-iot-sdk-csharp)
+[Azure IoT libraries for .NET](https://docs.microsoft.com/de-de/dotnet/api/overview/azure/iot)
