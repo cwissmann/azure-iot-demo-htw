1db7506d7f4918d8e2904827956904271f78b416
cwissmann <christian.wissmann@zeiss.com>
2022-04-21 08:04:53 +0200

    update versions

--- backend-function/IotHubTrigger.cs
+++ backend-function/IotHubTrigger.cs
@@ -1,11 +1,10 @@
-using IoTHubTrigger = Microsoft.Azure.WebJobs.EventHubTriggerAttribute;
-
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Azure.EventHubs;
 using System.Text;
 using System.Net.Http;
 using Microsoft.Extensions.Logging;
+
+using IoTHubTrigger = Microsoft.Azure.WebJobs.EventHubTriggerAttribute;
 
 namespace backend_function
 {
@@ -14,7 +13,7 @@ namespace backend_function
         private static HttpClient client = new HttpClient();
         
         [FunctionName("IotHubTrigger")]
-        public void Run([IoTHubTrigger("messages/events", Connection = "IOT_EVENTHUB_ENDPOINT")]EventData message, ILogger log)
+        public void Run([IoTHubTrigger("messages/events", Connection = "IOT_EVENTHUB_ENDPOINT")] EventData message, ILogger log)
         {
             log.LogInformation($"C# IoT Hub trigger function processed a message: {Encoding.UTF8.GetString(message.Body.Array)}");
         }
